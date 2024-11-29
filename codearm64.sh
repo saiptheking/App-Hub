8f3c575bfd341ac7db1a06ca9f03aa75d9637e10
saip_the_king <86388371+saiptheking@users.noreply.github.com>
2024-11-29 02:55:57 +0000

    Code Download

--- codearm64.sh
+++ codearm64.sh
@@ -1,1 +1,4 @@
-wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-arm64
+mkdir code
+cd code
+wget https://github.com/microsoft/vscode/archive/refs/tags/1.95.3.tar.gz
+tar -xf 1.95.3.tar.gz
